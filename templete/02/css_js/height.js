41f3bf93c7ca40c387b99211bcbc73b32111149c
Charlie <1500023570@qq.com>
2019-07-12 10:21:25 +0800

    1
    1

--- templete/02/css_js/height.js
+++
@@ -1,9 +0,0 @@
-function iframeResize(){
-
-var PageHight = document.body.scrollHeight + 30; // ページの高さを取得
-
-window.parent.document.getElementById('disp').style.height = PageHight + 'px'; // iframeの高さを変更
-
-}
-
-window.onload = iframeResize;
