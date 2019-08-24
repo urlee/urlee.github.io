98d5710236acf5eff2ae7ae3933254c241e9e75d
Charlie <1500023570@qq.com>
2019-08-24 09:25:37 +0800

    points

---
+++ points/points/js/data.js
@@ -0,0 +1,35 @@
+var oTgt = ["_blank","_top","_self","_search","_parent"];
+var myJson = [{
+	"url":"http://huaban.com/",
+	"src":"img/bnr_slider_01.jpg",
+	"txt":"花瓣网_陪你做生活的设计师"
+},{
+	"url":"http://www.xueui.cn/",
+	"src":"img/bnr_slider_02.jpg",
+	"txt":"学UI网—UI设计师学习教程平台"
+},{
+	"url":"http://www.uisdc.com/",
+	"src":"img/bnr_slider_03.jpg",
+	"txt":"优设网-设计师交流学习平台-听讲座，聊设计，找素材，尽在优设网"
+},{
+	"url":"https://www.csdn.net/",
+	"src":"img/bnr_slider_04.jpg",
+	"txt":"CSDN-专业IT技术社区"
+},{
+	"url":"https://www.w3cschool.cn/",
+	"src":"img/bnr_slider_05.jpg",
+	"txt":"w3cschool - 学编程，从w3cschool开始"
+},{
+	"url":"http://www.58pic.com/",
+	"src":"img/bnr_slider_06.jpg",
+	"txt":"千图网_专注免费设计素材下载的网站_免费设计图片素材中国"
+},{
+	"url":"https://www.behance.net/",
+	"src":"img/bnr_slider_07.jpg",
+	"txt":"Behance :: Behance 作品精选"
+},{
+	"url":"http://www.iconfont.cn/",
+	"src":"img/bnr_slider_08.jpg",
+	"txt":"Iconfont-阿里巴巴矢量图标库"
+}];
+	
