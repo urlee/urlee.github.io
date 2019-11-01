416dcc99952c3ba8ed6aeb06deec9bd0462530c0
Charlie <15092214875@163.com>
2019-11-01 22:07:00 +0800

    index

---
+++ common/js/header.js
@@ -0,0 +1,21 @@
+document.writeln("<div id=\'header\'>");
+document.writeln("    <div class=\'w\'>");
+document.writeln("        <div id=\'logo\'><a href=\'index.html\'><img src=\'./common/img/logo.png\' alt=\'榆畔\'></a></div>");
+document.writeln("        <ul id=\'g_icon\'>");
+document.writeln("            <li><a href=\'javascript:;\'><i class=\'fa fa-qq\'></i></a></li>");
+document.writeln("            <li><a href=\'javascript:;\'><i class=\'fa fa-wechat\'></i></a></li>");
+document.writeln("            <li><a href=\'javascript:;\'><i class=\'fa fa-weibo\'></i></a></li>");
+document.writeln("        </ul>");
+document.writeln("        <ul id=\'g_main\'>");
+document.writeln("            <li><a class=\'active\' href=\'./index.html\' target=\'_top\'>首页</a></li>");
+document.writeln("            <li><a href=\'./i_product.html\' target=\'_top\'>作品</a></li>");
+document.writeln("            <li><a href=\'./i_daily.html\' target=\'_top\'>日志</a></li>");
+document.writeln("            <li><a href=\'./i_about.html\' target=\'_top\'>关于</a></li>");
+document.writeln("            <li><a href=\'./i_contact.html\' target=\'_top\'>联系</a></li>");
+document.writeln("        </ul>");
+document.writeln("        ");
+document.writeln("    </div>");
+document.writeln("</div>");
+
+
+document.writeln("<script src=\'./common/js/jquery-3.2.1.min.js\'></script>");
